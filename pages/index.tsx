73bdca509c36fe5c5848eb3aff451b84131ab0ee
polbac <polbac@gmail.com>
2024-07-17 19:43:42 -0300

    jpg

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <title>A R W Y -- Nube Rosa</title>
 
       <meta property="og:title" content="ARWY NUBE ROSA" />
-      <meta property="og:image" content={"/shared.png"} />
+      <meta property="og:image" content={"/shared.jpg"} />
       <meta property="og:description" content={"música, bruja, performer"} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
