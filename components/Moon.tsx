f20658bc2978f24d4878f437d4ca0daaf4746d5a
polbac <polbac@gmail.com>
2024-08-17 22:20:25 -0300

    .

--- components/Moon.tsx
+++ components/Moon.tsx
@@ -1,24 +1,39 @@
+import { getRandomXYPositions } from "@/utils/browser";
 import { Moon as MoonPhase, Hemisphere } from "lunarphase-js";
 import { useState, useEffect } from "react";
+import Draggable from "react-draggable";
 
 export const Moon = () => {
+  const { x: startX, y: startY } = getRandomXYPositions();
   const [position, setPosition] = useState<Array<number>>([]);
   useEffect(() => {
-    setPosition([Math.random() * 100, Math.random() * 100]);
-  }, []);
+    setPosition([startX, startY]);
+  }, [startX, startY]);
+
+  if (!position[0]) {
+    return null;
+  }
+
   return (
-    <div
-      style={{
-        position: "absolute",
-        fontSize: "70px",
-        top: `${position[0]}%`,
-        left: `${position[0]}%`,
-        filter: "grayscale(1)",
+    <Draggable
+      defaultPosition={{
+        x: position[0],
+        y: position[1],
       }}
     >
-      {MoonPhase.lunarPhaseEmoji(new Date(), {
-        hemisphere: Hemisphere.SOUTHERN,
-      })}
-    </div>
+      <div
+        style={{
+          fontSize: "70px",
+
+          filter: "grayscale(1)",
+        }}
+      >
+        <div style={{ position: "absolute" }}>
+          {MoonPhase.lunarPhaseEmoji(new Date(), {
+            hemisphere: Hemisphere.SOUTHERN,
+          })}
+        </div>
+      </div>
+    </Draggable>
   );
 };
